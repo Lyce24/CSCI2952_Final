29da3bea713b78f75ad6c09e4df8bff355b903b7
mspancho <manan_pancholy@brown.edu>
2025-12-11 01:08:11 -0500

    added mimic query script

---
+++ Scripts/medmod/query_mimic_cxr.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from skmultilearn.model_selection import iterative_train_test_split
+import os
+import shutil
+import subprocess
+
+medmod_fp = "/users/mspancho/Downloads/MedMod/mimic4extract/data/final/medmod_final.csv"
+
+cmd = [
+    "wget",
+    "-r", "-N", "-c", "-np",
+    "--user=mspancho",
+    "--password=MIMICsp27!",
+    "https://physionet.org/files/mimic-cxr-jpg/2.1.0/files/"
+]
+
+def main():
+    print("Starting script")
+
+    medmod_df = pd.read_csv(medmod_fp)
+    # train_df, val_df, test_df = label_split(medmod_df)
+
+    for row in medmod_df.itertuples():
+        cmd_cxr = cmd.copy()
+        cxr_path = str(row.cxr_path)
+        cxr_path_correct = cxr_path[:4] + "p" + cxr_path[4:]
+        cmd_cxr[7] = cmd_cxr[7] + str(cxr_path_correct)
+        subprocess.run(cmd_cxr, check=True)
+
+    print("Script complete")
+
+
+
+if __name__ == "__main__":
+    main()
